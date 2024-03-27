354a3f8cc76993493e33c142b104726fe7c90b33
largepizza <nphaklides@hotmail.com>
2024-03-26 21:53:24 -0700

    ASCEND python service

--- install_service.sh
+++ install_service.sh
@@ -1,27 +1,47 @@
 #!/bin/bash
 
 # Define the source and destination paths
-SOURCE_FILE="rtl_record.service"
+RTL_SOURCE_FILE="rtl_record.service"
+ASCEND_SOURCE_FILE="ascend.service"
+
 DESTINATION_PATH="/etc/systemd/system/"
 
-# Check if the source file exists
-if [ ! -f "$SOURCE_FILE" ]; then
-    echo "Error: $SOURCE_FILE does not exist."
+# Check if the rtl source file exists
+if [ ! -f "$RTL_SOURCE_FILE" ]; then
+    echo "Error: $RTL_SOURCE_FILE does not exist."
     exit 1
 fi
 
-# Copy the service file to the systemd directory
-echo "Copying $SOURCE_FILE to $DESTINATION_PATH"
-sudo cp "$SOURCE_FILE" "${DESTINATION_PATH}${SOURCE_FILE}"
+# Check if the ascend source file exists
+if [ ! -f "$ASCEND_SOURCE_FILE" ]; then
+    echo "Error: $ASCEND_SOURCE_FILE does not exist."
+    exit 1
+fi
+
+
+# Copy the rtl service file to the systemd directory
+echo "Copying $RTL_SOURCE_FILE to $DESTINATION_PATH"
+sudo cp "$RTL_SOURCE_FILE" "${DESTINATION_PATH}${RTL_SOURCE_FILE}"
+
+# Copy the ascend service file to the systemd directory
+echo "Copying $ASCEND_SOURCE_FILE to $DESTINATION_PATH"
+sudo cp "$ASCEND_SOURCE_FILE" "${DESTINATION_PATH}${ASCEND_SOURCE_FILE}"
+
 
 # Reload systemd to recognize the new service
 echo "Reloading systemd daemon..."
 sudo systemctl daemon-reload
 
-# Enable the service to start on boot
-echo "Enabling the $SOURCE_FILE service..."
-sudo systemctl enable "$SOURCE_FILE"
+# Enable the rtl service to start on boot
+echo "Enabling the $RTL_SOURCE_FILE service..."
+sudo systemctl enable "$RTL_SOURCE_FILE"
+
+# Enable the ascend service to start on boot
+echo "Enabling the $ASCEND_SOURCE_FILE service..."
+sudo systemctl enable "$ASCEND_SOURCE_FILE"
 
 # Provide instructions to start the service now, if desired
-echo "The service has been installed. To start the service, run:"
-echo "    sudo systemctl start $SOURCE_FILE"
+echo "The service has been installed. To start the rtl service, run:"
+echo "    sudo systemctl start $RTL_SOURCE_FILE"
+echo "To start the ascend service, run:"
+echo "    sudo systemctl start $ASCEND_SOURCE_FILE"
