0db3c1d7a7ab14b181abf359799a3576c0874b0e
largepizza <nphaklides@hotmail.com>
2024-03-26 22:22:41 -0700

    Console logging and error fix

--- start_main.sh
+++ start_main.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+
+# Get the current date and time for the unique file name
+timestamp=$(date '+%Y-%m-%d_%H-%M-%S')
+
+# Full path to where you want to store your log files
+log_path="/home/eclipse/vlf/logs/"
+
+# Create the log file with the current timestamp
+log_file="${log_path}log_${timestamp}.txt"
+
 # Activate the virtual environment
 source /home/eclipse/vlf/ASCEND_Eclipse_PI/.venv/bin/activate
 
-# Run your Python script
-python /home/eclipse/vlf/ASCEND_Eclipse_PI/main.py
+# Run Python script
+python /home/eclipse/vlf/ASCEND_Eclipse_PI/main.py >> $log_file
